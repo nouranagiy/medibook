e3ed445816bd41adc9223c712aa78940e6a48fb0
nora <nora.nagiy@gmail.com>
2026-08-03 02:07:52 +0300

    Setup project structure and theme foundation

---
+++ lib/app/theme/colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+class AppColors {
+  static const Color primary = Color(0xFF4F8EF7);
+  static const Color secondary = Color(0xFF00C9A7);
+  static const Color background = Color(0xFFF8FAFC);
+  static const Color white = Colors.white;
+  static const Color black = Color(0xFF1F2937);
+  static const Color grey = Color(0xFF6B7280);
+  static const Color lightGrey = Color(0xFFE5E7EB);
+  static const Color error = Color(0xFFEF4444);
+}
